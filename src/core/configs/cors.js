9693b4fd3e3b80aba62e1fe05fab4398fa5027f2
Trungnc273 <160880795+Trungnc273@users.noreply.github.com>
2025-11-02 21:27:35 +0700

    fix cors

--- src/core/configs/cors.js
+++ src/core/configs/cors.js
@@ -1,7 +1,19 @@
 import environment from "./environment.js";
 
+const whitelist = [
+  environment.clientUrl, // Web App (localhost or deployed)
+  null, // Cho phép Expo Mobile app gọi API
+];
+
 const corsOptions = {
-  origin: environment.clientUrl,
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      console.log("❌ Blocked CORS:", origin);
+      callback(new Error("CORS Not allowed"));
+    }
+  },
   credentials: true,
 };
 
